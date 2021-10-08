30b123b8ee6279f2b72b94088d785e4b30d15e32
makur <makur@DESKTOP-RGPRK2U>
2021-10-08 22:35:01 +0300

    Добавьте файлы проекта.

---
+++ GetAsciiPicture/AsciiConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GetAsciiPicture
+{
+    public static class Extensions
+    {
+        public static void ToGray(this Bitmap bitmap)
+        {
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    var pixel = bitmap.GetPixel(j, i);
+                    int average = (pixel.R + pixel.G + pixel.B) / 3;
+                    bitmap.SetPixel(j, i, Color.FromArgb(pixel.A, average, average, average));
+                }
+            }
+        }
+    }
+    public class AsciiConverter
+    {
+        private readonly Bitmap bitmap1;
+        private readonly char[] asciiTable1 = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+        public AsciiConverter(Bitmap bitmap)
+        {
+            bitmap1 = bitmap;
+        }
+        public char[][] Convert()
+        {
+            var result = new char[bitmap1.Height][];
+            for (int i = 0; i < bitmap1.Height; i++)
+            {
+                result[i] = new char[bitmap1.Width];
+                for (int j = 0; j < bitmap1.Width; j++)
+                {
+                    int mapIndex = (int)Map(bitmap1.GetPixel(j, i).R, 0, 255, 0, asciiTable1.Length - 1);
+                    result[i][j] = asciiTable1[mapIndex];
+                }
+            }
+            return result;
+        }
+        private float Map(float map, float start1, float stop1, float start2, float stop2)
+        {
+            return ((map - start1) / (stop1 - start1) * (stop2 - start2) + start2);
+        }
+    }
+    }
---
+++ GetAsciiPicture/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace GetAsciiPicture
+{
+    class Program
+    {
+        static void GetPrintBitMap(Bitmap bitmap)
+        {
+            bitmap = ResizeBitMap(bitmap);
+            bitmap.ToGray();
+            var converter = new AsciiConverter(bitmap);
+            var rows = converter.Convert();
+            foreach (var row in rows)
+            {
+                Console.WriteLine(row);
+            }
+            Console.SetCursorPosition(0, 0);
+
+        }
+        private static Bitmap ResizeBitMap(Bitmap bitmap)
+        {
+            int maxWidth = 350;
+            var newHeight = bitmap.Height / 2 * maxWidth / bitmap.Width;
+            if ((bitmap.Width > maxWidth) || (bitmap.Height > newHeight))
+            {
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            }
+            return bitmap;
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFile = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.jpg; *.png"
+            };           
+           
+            while (true)
+            {
+                if (openFile.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+                Bitmap bitmap = new Bitmap(openFile.FileName);
+                GetPrintBitMap(bitmap);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
